a6d912398fd03e381153c02f1ba26510a02e5673
Samy Alzahrani <samy@jehazy.com>
2019-02-01 18:17:46 +0300

    minor validation

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const btn = document.querySelector('#chat');
 const input = document.querySelector('#tel');
-btn.onclick = () => {
-	const {value} = input;
-	window.location =`https://wa.me/${value.replace(/^05/, '9665')}`
+
+const checkInput = el => {
+	const { value } = el;
+    if (
+        (value.indexOf('05') === 0 && value.trim().length === 10) ||
+        (value.indexOf('966') === 0 && value.trim().length === 12)
+    ) {
+		btn.disabled = '';
+    } else {
+		btn.disabled = 'true';
+    }
 }
+
+checkInput(input);
+
+input.addEventListener('keyup', evt => {
+	checkInput(evt.target);
+});
+
+btn.onclick = () => {
+    const { value } = input;
+    window.location = `https://wa.me/${value.replace(/^05/, '9665')}`;
+};
